6403cb139bab64a11d7b967b7eea6cf64de339e5
alexandreBruneau <alexandrebruneau2005@protonmail.com>
2023-10-06 09:39:26 +0200

    On branch main
      Initial commit
    
      Changes to be committed:
    	new file:   .gitignore
        new file:   README.md
    	new file:   calcul_salaire/pom.xml
    	new file:   pom.xml
    	new file:   src/main/java/fr/dampierre/App.java
    	new file:   src/test/java/fr/dampierre/AppTest.java

---
+++ src/main/java/fr/dampierre/App.java
@@ -0,0 +1,20 @@
+package fr.dampierre;
+
+import java.util.Scanner;
+
+public class App {
+
+    public static void main(String[] args) {
+        int salaire = 400;
+        int prime = 250;
+        int quota = 10;
+        System.out.println("Combien de ventes cette semaine ?");
+        Scanner clavier = new Scanner(System.in);
+        int nbDeVentes = clavier.nextInt();
+        clavier.close();
+        if (nbDeVentes >= quota) {
+            salaire = salaire + prime;
+        }
+        System.out.println("Le salaire hebdo est de : " + salaire);
+    }
+}
